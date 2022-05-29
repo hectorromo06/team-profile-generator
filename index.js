4958910f22ff382abfa4f53dd3bca3971c5a5c1a
Hector Romo <hectorommmo@gmail.com>
2022-05-29 00:18:39 -0700

    generate manager card html

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const {
 } = require('./utils/generate-site');
 const generatePage = require('./src/js/generate-html');
 
-const promptUser = () => {
+const promptManager = () => {
     return inquirer
         .prompt([{
             type: 'input',
-            name: 'name',
+            name: 'managerName',
             message: 'Please enter the managers name: (Required)',
             validate: managerNameInput => {
                 if (managerNameInput) {
@@ -55,28 +55,178 @@ const promptUser = () => {
                     return false;
                 }
             }
-        }]);
+        }])
 };
 
-promptUser()
-    .then(promptEngineer)
-    .then(promptIntern)
-    .then(resp => {
-        console.log(resp);
+const promptTeam = teamData => {
+    return inquirer
+        .prompt([{
+            type: 'list',
+            name: 'member',
+            message: 'Select from the following list:',
+            choices: ['Engineer', 'Intern', 'Finish Team']
+        }])
+        .then(resp => {
+            if (resp.member == 'Engineer') {
+                return promptEngineer(teamData);
+            } else if (resp.member == 'Intern') {
+                return promptIntern(teamData);
+            } else if (resp.member == 'Finish Team') {
+                return teamData;
+            }
+        });
+};
+
+const promptEngineer = teamData => {
+    // If no 'engineers' array property, create one
+    if (!teamData.engineers) {
+        teamData.engineers = [];
+    }
+    console.log(`
+    ===================
+    Enter Engineer Info
+    ===================
+    `);
+    return inquirer
+        .prompt([{
+            type: 'input',
+            name: 'engName',
+            message: 'Please enter the engineers name: (Required)',
+            validate: engineerNameInput => {
+                if (engineerNameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the engineers name!');
+                    return false;
+                }
+            }
+        }, {
+            type: 'number',
+            name: 'id',
+            message: 'Please enter the engineers employee ID : (Required)',
+            validate: engineerIdInput => {
+                if (engineerIdInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the engineers employee ID!');
+                    return false;
+                }
+            }
+        }, {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter the engineers email: (Required)',
+            validate: engineerEmailInput => {
+                if (engineerEmailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the engineers email!');
+                    return false;
+                }
+            }
+        }, {
+            type: 'input',
+            name: 'github',
+            message: 'Please enter the engineers GitHub username: (Required)',
+            validate: engineerGithubInput => {
+                if (engineerGithubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the engineers GitHub username!');
+                    return false;
+                }
+            }
+        }])
+        .then(resp => {
+            teamData.engineers.push(resp);
+            return promptTeam(teamData);
+        })
+};
+
+const promptIntern = teamData => {
+    // If no 'interns' array property, create one
+    if (!teamData.interns) {
+        teamData.interns = [];
+    }
+    console.log(`
+    ==================
+    Enter Interns Info
+    ==================
+    `);
+    return inquirer
+        .prompt([{
+            type: 'input',
+            name: 'internName',
+            message: 'Please enter the interns name: (Required)',
+            validate: internNameInput => {
+                if (internNameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the interns name!');
+                    return false;
+                }
+            }
+        }, {
+            type: 'number',
+            name: 'id',
+            message: 'Please enter the interns employee ID : (Required)',
+            validate: internIdInput => {
+                if (internIdInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the interns employee ID!');
+                    return false;
+                }
+            }
+        }, {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter the interns email: (Required)',
+            validate: internEmailInput => {
+                if (internEmailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the interns email!');
+                    return false;
+                }
+            }
+        }, {
+            type: 'input',
+            name: 'github',
+            message: 'Please enter the interns school name: (Required)',
+            validate: internSchoolInput => {
+                if (internSchoolInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the interns school name!');
+                    return false;
+                }
+            }
+        }])
+        .then(resp => {
+            teamData.interns.push(resp);
+            return promptTeam(teamData);
+        })
+};
+
+promptManager()
+    .then(promptTeam)
+    // .then(resp => {
+    //     console.log(resp);
+    // })
+    .then(teamData => {
+        return generatePage(teamData);
     })
-// .then(teamData => {
-//     return generatePage(teamData);
-// })
-// .then(pageHTML => {
-//     return writeFile(pageHTML);
-// })
-// .then(writeFileResponse => {
-//     console.log(writeFileResponse);
-//     return copyFile();
-// })
-// .then(copyFileResponse => {
-//     console.log(copyFileResponse);
-// })
-// .catch(err => {
-//     console.log(err);
-// });
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
+    });
